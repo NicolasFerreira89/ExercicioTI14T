49300ff22cf526257a35ab5bb48d407743a1f4a3
NICOLAS DE SOUSA FERREIRA <nicolas.sferreira5@senacsp.edu.br>
2022-04-13 14:37:23 -0300

    first commit

---
+++ ExerciciosTI14T/ControlExercicios.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI14T
+{
+    class ControlExercicios
+    {
+        //Ligar as duas classes
+        ModelExercicios model;
+        private int opcao;
+        public double bas;
+        public double altura;
+        
+        public ControlExercicios()
+        {
+            model = new ModelExercicios();//Chamando o construtor da classe Model Exercicios
+        }//fim do construtor
+
+        public int ConsultarOpcao
+        {
+            get
+            {
+                return opcao;
+            }
+            set
+            {
+                this.opcao = value;
+            }
+        }
+        public void Menu()
+        {
+            Console.WriteLine("---- Menu ----" +
+                             "\n1. Exercício 01" +
+                             "\n2. Exercício 02" +
+                             "\n3. Exercício 03");
+            Console.WriteLine("Escolha uma das opções acima: ");
+            ConsultarOpcao = Convert.ToInt32(Console.ReadLine());
+        }//fim do menu
+
+        public void Executar()
+        {
+            Menu();
+            //Executar a ação
+            switch (ConsultarOpcao)
+            {
+                case 1:
+                    Console.WriteLine(model.Exercicio01());
+                    break;
+                case 2:
+                    Console.WriteLine("Informe um numero");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("O antecessor de " + num + "é" + model.Exercicio02(num));
+                    break;
+                case 3:
+                    do
+                    {
+                        Console.WriteLine("Informe a base:");
+                         bas = Convert.ToDouble(Console.ReadLine());
+                        if (bas <= 0)
+                        {
+                            Console.WriteLine("Base digitada não é valida, digite novamente");
+
+                        }
+                    } while (bas <= 0);
+                    do
+                    {
+                        Console.WriteLine("Infrome a altura:");
+                        double altura = Convert.ToDouble(Console.ReadLine());
+                        if (altura <= 0)
+                        {
+                            Console.WriteLine("Altura digitada não é valida, tente novamente");
+                        }
+                    } while (altura <= 0);
+                    Console.WriteLine("A área do retangulo é: " + model.Exercicio03(bas, altura));
+                    break;
+                case 4:
+                    Console.WriteLine("Digite sua idade:");
+                    int idade = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Digite os meses:");
+                    int meses = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digiete os dias:");
+                    int dias = Convert.ToInt32(Console.ReadLine());
+                    if (dias < 0)
+                    {
+                        Console.WriteLine("Numeros de dias invlido!");
+                    }
+                    Console.WriteLine("A sua idade em dias é:" + model.Exercicio04(idade, meses, dias) + " dias");
+                    break;
+                 
+
+
+                default:
+                    Console.WriteLine("Código digitado não é valido!");
+                    break;
+               
+
+            }//fim do switch
+        }//fim do executar
+    }//fim da classe
+}//fim do projeto
---
+++ ExerciciosTI14T/ModelExercicios.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI14T
+{
+    class ModelExercicios
+    {
+        //Declaração de variáveis
+        private int A;
+        private int B;
+        private int C;
+        
+        //Declarar o construtor
+        public ModelExercicios()
+        {
+            ConsultarA = 10;
+            ConsultarB = 20;
+            ConsultarC = 0;
+            
+        }//fim do construtor
+
+        public int ConsultarA
+        {
+            get
+            {
+                return A;
+            }
+            set
+            {
+                this.A = value;
+            }
+        }//fim do consultarA
+
+        public int ConsultarB
+        {
+            get
+            {
+                return B;
+            }
+            set
+            {
+                this.B = value;
+            }
+        }//fim do consultarB
+
+        public int ConsultarC
+        {
+            get
+            {
+                return C;
+            }
+            set
+            {
+                this.C = value;
+            }
+        }//fim do consultarC
+
+        //Método Exercício01
+        public string Exercicio01()
+        {
+            ConsultarC = ConsultarA;
+            ConsultarA = ConsultarB;
+            ConsultarB = ConsultarC;
+            return "A: " + ConsultarA + "\nB: " + ConsultarB;
+        }//fim do métodoExercicio01
+
+        public int Exercicio02(int num)
+        {
+            
+            return num - 1;
+           
+
+        }
+
+
+        public double Exercicio03(double bas, double altura)
+        {
+            return bas * altura;
+        }
+
+        public int Exercicio04(int idade, int meses, int dias)
+        {
+            return (meses * 30) + (idade * 365) + dias;
+        }
+        
+
+             public int Exercicio05(int eleitores, int vtb, int vtn, int vtv)
+             {
+                
+             }
+        
+
+        
+               
+
+            
+            
+
+            
+
+    }//fim da classe
+}//fim do projeto
